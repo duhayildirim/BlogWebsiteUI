bb27a17255e6978b5bffab2504bea694e17c6bcb
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-03-03 16:33:31 +0300

    error message

--- src/Components/UserSignupPage.js
+++ src/Components/UserSignupPage.js
@@ -9,13 +9,17 @@ class UserSignupPage extends React.Component {
     email: null,
     password: null,
     passwordCheck: null,
-    pandingApiCall: false
+    pandingApiCall: false,
+    errors: {}
   }
 
   onChangeValue = e => {
     const { id, value } = e.target
+    const errors = { ...this.state.errors }
+    errors[id] = undefined
     this.setState({
-      [id]: value
+      [id]: value,
+      errors
     })
   }
 
@@ -29,15 +33,17 @@ class UserSignupPage extends React.Component {
     try {
       const response = await signUp(body);
     } catch (error) {
-
+      if (error.response.data.validationErrors) {
+        this.setState({ errors: error.response.data.validationErrors })
+      }
     }
     this.setState({ pandingApiCall: false })
-      
-      // signUp(body).then((response) => {
-      //   this.setState({ pandingApiCall: false })
-      // }).catch((error) => {
-      //   this.setState({ pandingApiCall: false })
-      // })
+
+    // signUp(body).then((response) => {
+    //   this.setState({ pandingApiCall: false })
+    // }).catch((error) => {
+    //   this.setState({ pandingApiCall: false })
+    // })
   }
 
   render() {
@@ -50,29 +56,34 @@ class UserSignupPage extends React.Component {
         <form className="p-3 mt-3">
           <div className="form-field d-flex align-items-center">
             <span className="far fa-user"></span>
-            <input id="userName" onChange={this.onChangeValue} className="input" type="text" placeholder="Name" />
+            <input id="userName" onChange={this.onChangeValue} className="input" type="text" placeholder="Name*" />
+          </div>
+          {
+            this.state.errors.userName && <div className="alert alert-danger" role="alert">
+              Name field cannot be null.
+            </div>
+          }
+          <div className="form-field d-flex align-items-center">
+            <span className="far fa-user"></span>
+            <input id="displayName" onChange={this.onChangeValue} className="input" type="text" placeholder="Display name*" />
           </div>
           <div className="form-field d-flex align-items-center">
             <span className="far fa-user"></span>
-            <input id="displayName" onChange={this.onChangeValue} className="input" type="text" placeholder="Display name" />
-          </div>
-          <div className="form-field d-flex align-items-center">
-            <span className="far fa-user"></span>
-            <input id="email" onChange={this.onChangeValue} className="input" type="text" placeholder="Email" />
+            <input id="email" onChange={this.onChangeValue} className="input" type="text" placeholder="Email*" />
           </div>
           <div className="form-field d-flex align-items-center">
             <span className="fas fa-key"></span>
-            <input id="password" onChange={this.onChangeValue} className="input" type="password" placeholder="Password" />
+            <input id="password" onChange={this.onChangeValue} className="input" type="password" placeholder="Password*" />
           </div>
           <div className="form-field d-flex align-items-center">
             <span className="fas fa-key"></span>
-            <input id="passwordCheck" onChange={this.onChangeValue} className="input" type="password" placeholder="Password Check" />
+            <input id="passwordCheck" onChange={this.onChangeValue} className="input" type="password" placeholder="Password Check*" />
           </div>
           <button disabled={this.state.pandingApiCall} onClick={this.submit} className="btn mt-3">
             {
               !this.state.pandingApiCall ? "Submit" :
-                <div class="spinner-border text-secondary" role="status">
-                  <span class="sr-only"></span>
+                <div className="spinner-border text-secondary" role="status">
+                  <span className="sr-only"></span>
                 </div>
             }
 
